752201461d37adaed1e7cffd0fc2d8266f0659b3
maikeldf <maikeldf@gmail.com>
2020-12-13 23:06:56 +0000

    Task a78176bb solved

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 
+"""
+Student name(s): Maikel Dal Farra
+Student ID(s): 20235851
+https://github.com/maikeldf/ARC.git
+
+Taks:
+    ded97339
+    a2fd1cf0
+    a78176bb
+"""
+
 import os, sys
 import json
 import numpy as np
@@ -10,23 +21,84 @@ import re
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
-"""def solve_6a1e5592(x):
-    return x
 
-def solve_b2862040(x):
-    return x"""
+def paint_inf(coord, x, color):
+    x[coord[0] , coord[1]] = color
+    
+    if coord[0] >= x.shape[0] - 1 or coord[1] >= x.shape[1] - 1 :
+        return 0
+
+    coord[0] += 1
+    coord[1] += 1
+    paint_inf(coord, x, color)
+
+    return 0
+
+def paint_sup(coord, x, color):
+
+    x[coord[0] , coord[1]] = color
+
+    if coord[0] <= 0 or coord[1] > x.shape[1] or coord[1] <= 0:
+        return 0
+
+    coord[0] -= 1
+    coord[1] -= 1
+    paint_sup(coord, x, color)
+
+    return 0
+
+def solve_a78176bb(x):
+    t = tuple(zip(np.where(np.array(x)==5)[0], np.where(np.array(x)==5)[1]))
+
+    # Find top and bottom corner element
+    top = []
+    for t_ in t:
+        if  x[t_[0]-1, t_[1]] == 0 and x[t_[0], t_[1]+1] == 0:
+            top = [t_[0],t_[1]]
+            break
+
+    bottom = []
+    for t_ in t:
+        if x[t_[0]+1, t_[1]] == 0 and x[t_[0], t_[1]-1] == 0:
+                bottom = [t_[0],t_[1]]
+                break
+
+    # The color that is not black and not gray
+    color = x[tuple(zip(np.where(np.array(x)!=5) 
+        and np.where(np.array(x)!=0)[0], np.where(np.array(x)!=5) 
+        and np.where(np.array(x)!=0)[1]))[0]]
+
+    if len(top):
+        paint_sup([top[0] - 1, top[1] + 1], x, color)
+        paint_inf([top[0] - 1, top[1] + 1], x, color)
+
+    if len(bottom):
+        paint_sup([bottom[0] + 1, bottom[1] - 1], x, color)
+        paint_inf([bottom[0] + 1, bottom[1] - 1], x, color)
+
+    def draw(p):
+        x[p] = 0
+
+    [draw(t_) for t_ in t]
+
+    return x
 
 def ded97339(coord, bricks, x):
     x[coord] = 0
     lost = []
 
+    # Stop when there is no more bricks to store
     if not np.where(np.array(x)==8)[0].size:
         return []
 
-    bricks.append([np.where(np.array(x)==8)[0][0], np.where(np.array(x)==8)[1][0]] if np.where(np.array(x)==8)[0].size else 0)
+    # Store all the bricks coordinates
+    bricks.append([np.where(np.array(x)==8)[0][0], np.where(np.array(x)==8)[1][0]] 
+        if np.where(np.array(x)==8)[0].size else 0)
 
+    # Recursively call to check the next brick
     lost = ded97339((bricks[-1][0], bricks[-1][1]), bricks, x)
 
+    # Starts from the last coodinate stored
     pointA = bricks.pop()
 
     def draw_point(a, b):
@@ -37,17 +109,24 @@ def ded97339(coord, bricks, x):
         [ draw_point(i, l[0][1][1]) for i in range(l[0][0][0], l[0][1][0]) if l[0][0][1] == l[0][1][1] ]
         x[l[0][1][0],l[0][1][1]] = 8
 
+    # Find the point B of the point A to draw a line
+    # Basically checking whether exist any brick with simetric coordinates.
     pointB = []
-    if np.where(np.array(bricks) == pointA[0])[0].size and bricks[np.where(np.array(bricks) == pointA[0])[0][0]][0] == pointA[0]:
-        pointB = [bricks[np.where(np.array(bricks) == pointA[0])[0][0]][0], bricks[np.where(np.array(bricks) == pointA[0])[0][0]][1]]
+    if np.where(np.array(bricks) == pointA[0])[0].size \
+        and bricks[np.where(np.array(bricks) == pointA[0])[0][0]][0] == pointA[0]:
+        pointB = [bricks[np.where(np.array(bricks) == 
+            pointA[0])[0][0]][0], bricks[np.where(np.array(bricks) == pointA[0])[0][0]][1]]
 
         draw_line([[pointB,pointA] if pointA>pointB else [pointA,pointB]], x)
 
-    if np.where(np.array(bricks) == pointA[1])[0].size and bricks[np.where(np.array(bricks) == pointA[1])[0][0]][1] == pointA[1]:
-        pointB = [bricks[np.where(np.array(bricks) == pointA[1])[0][0]][0], bricks[np.where(np.array(bricks) == pointA[1])[0][0]][1]]
+    if np.where(np.array(bricks) == pointA[1])[0].size \
+        and bricks[np.where(np.array(bricks) == pointA[1])[0][0]][1] == pointA[1]:
+        pointB = [bricks[np.where(np.array(bricks) == 
+            pointA[1])[0][0]][0], bricks[np.where(np.array(bricks) == pointA[1])[0][0]][1]]
 
         draw_line([[pointB,pointA] if pointA>pointB else [pointA,pointB]], x)
 
+    # Store the bricks that doesn't make part of any line
     if not pointB:
         lost.append(pointA)
 
@@ -60,6 +139,7 @@ def solve_ded97339(x):
     if np.where(np.array(x)==8)[0].size:
         bricks.append([np.where(np.array(x)==8)[0][0], np.where(np.array(x)==8)[1][0]] if np.where(np.array(x)==8)[0].size else 0)
         lost = ded97339((bricks[-1][0], bricks[-1][1]), bricks, x)
+        # Draw the loose bricks
         def draw(p): x[p[0],p[1]] = 8
         [draw(l) for l in lost if x[l[0],l[1]] == 0]
 
